05766849e42152e842538c09f2aa3fb67e69b829
Amonics <consultingiac24@gmail.com>
2026-09-01 02:37:49 +0200

    Remove live Stripe secret key from server.js
    Replaced live Stripe secret key with an empty string.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')('sk_live_REMOVED');
+const stripe = require('stripe')('');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
